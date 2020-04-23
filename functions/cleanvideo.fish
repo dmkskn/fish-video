acafac29887eaf4c89107a706b713ef843e082a1
dmkskn <dmksknn@gmail.com>
2020-04-23 13:22:15 +0300

    Add cleanvideo function

---
+++ functions/cleanvideo.fish
@@ -0,0 +1,89 @@
+function cleanvideo -d "Leave only what you need in the video container"
+
+    # By default it selects subtitles track 1 and audio track 1
+    set -l subtitles_indexies 1
+    set -l audio_indexies 1
+
+    set -l FFMPEG_PATH (command -v ffmpeg)
+    set -l loglevel "fatal"
+
+    # If there is no fish-getopts, throw an error
+    if not test (string split " " (functions --all) | grep getopts)
+        echo "fish-getopts is required (https://github.com/jorgebucaran/fish-getopts)." >&2
+        return 1
+    end
+
+    # Parse key value arguments
+    getopts $argv | while read -l key value
+        switch $key
+            case i input
+                set input $value
+            case o output
+                set output $value
+            case a audio-indexies
+                set audio_indexies $value
+            case s subtitles-indexies
+                set subtitles_indexies $value
+            case f ffmpeg
+                set FFMPEG_PATH $value
+            case l loglevel
+                set loglevel $value
+            case h help
+                echo "Keep in the video container only what you need."
+                echo ""
+                echo "Options:"
+                echo ""
+                echo "  -i --input                an input file path (required)"
+                echo "  -o --output               an output file path (required)"
+                echo "  -a --audio-indexies       audio track index in the file (default: 1)"
+                echo "  -s --subtitles-indexies   subtitle track index in the file (default: 1)"
+                echo "  -f --ffmpeg               path to the ffmpeg executable"
+                echo "  -l --loglevel             ffmpeg loglevel (default: fatal)"
+                echo ""
+                echo "Example:"
+                echo ""
+                echo '  cleanvideo --input "input.mkv" --output "output.mkv" --audio-indexies 3 --subtitles-indexies 3,2'
+
+                return
+        end
+    end
+
+    # If there is no ffmpeg executable, throw an error
+    if not test -e $FFMPEG_PATH
+        echo "ffmpeg is required (https://www.ffmpeg.org)." >&2
+        return 1
+    end
+
+    # If input path was not set, throw an error
+    if not set -q input
+        echo "Error: `input` was not set. See help." >&2
+        return 1
+    end
+
+    # If output path was not set, throw an error
+    if not set -q output
+        echo "Error: `output` was not set. See help." >&2
+        return 1
+    end
+
+    # Collect -map arguments with correct indexies (ffmprobe returns correct map index)
+
+    set -l maps "-map" "0:0"
+
+    for ai in (string split , $audio_indexies)
+        set ai (math $ai - 1)
+        set ai (ffprobe $input -hide_banner -loglevel panic -select_streams a:$ai -show_entries stream=index -of csv=s=x:p=0)
+        set ai "0:$ai"
+        set maps $maps "-map" $ai
+    end
+
+    for si in (string split , $subtitles_indexies)
+        set si (math $si - 1)
+        set si (ffprobe $input -hide_banner -loglevel panic -select_streams s:$si -show_entries stream=index -of csv=s=x:p=0)
+        set si "0:$si"
+        set maps $maps "-map" $si
+    end
+
+    $FFMPEG_PATH -hide_banner -loglevel $loglevel -i "$input" -c:a copy -c:v copy -c:s copy $maps -y "$output"
+    return 0
+end
---
+++ tests/test_cleanvideo.fish
@@ -0,0 +1,45 @@
+@mesg "$current_filename"
+
+
+function setup
+    source (pwd)/functions/cleanvideo.fish
+end
+
+function teardown
+    set matches /tmp/log.?????
+    command --quiet rm $matches
+    set --erase tmp_log
+end
+
+
+@test "throws and error code 1 if `--input` was not set" (
+    cleanvideo --output output.mkv &> /dev/null
+) $status -eq 1
+
+
+@test "throws and error if `--input` was not set" (
+    set -g tmp_log (command mktemp /tmp/log.XXXXX)
+    cleanvideo --output output.mkv &> $tmp_log
+) (cat $tmp_log) = "Error: `input` was not set. See help."
+
+
+@test "throws and error code 1 if `--output` was not set" (
+    cleanvideo --input input.mkv &> /dev/null
+) $status -eq 1
+
+
+@test "throws and error if `--output` was not set" (
+    set -g tmp_log (command mktemp /tmp/log.XXXXX)
+    cleanvideo --input input.mkv &> $tmp_log
+) (cat $tmp_log) = "Error: `output` was not set. See help."
+
+
+@test "throws and error code 1 if ffmpeg was not found" (
+    cleanvideo --input input.mkv --output output.mkv -f /path/to/the/ffmpeg &> /dev/null
+) $status -eq 1
+
+
+@test "throws and error if ffmpeg was not found" (
+    set -g tmp_log (command mktemp /tmp/log.XXXXX)
+    cleanvideo --input input.mkv --output output.mkv -f /path/to/the/ffmpeg &> $tmp_log
+) (cat $tmp_log) = "ffmpeg is required (https://www.ffmpeg.org)."
